a043618f99427383e5fd5ad0961c599ed0d93e15
Ifeoluwa Frank Idowu <ifeoluwa.frank@yahoo.com>
2022-07-01 16:48:56 -0700

    All

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prins every decimal digit
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+
+}
