63ff576109613929b568376975c6102843ef8fb4
Ifeoluwa Frank Idowu <ifeoluwa.frank@yahoo.com>
2022-07-01 17:57:39 -0700

    hhh

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Thisprogram prints comma-separated
+ * combined digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (j = 48; j < 56; j++)
+	{
+		for (j = i + 1; j < 57; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				iif (i != 55)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				else
+				{
+					putchar(10);
+				}
+			}
+		}
+	}
+	return (0);
+}
